ee1628b6a51e04c0fe60093f4d0ff8cd98cceedf
Dawid <Dawid@Gideon>
2017-06-22 23:29:46 +0200

    ScheduleView

---
+++ vacation-web/src/application/vacation/web/bean/ScheduleView.java
@@ -0,0 +1,93 @@
+package application.vacation.web.bean;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+ 
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.ScheduleEntryResizeEvent;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
+ 
+@ManagedBean 
+@ViewScoped
+public class ScheduleView implements Serializable {
+ 
+    private ScheduleModel eventModel;
+        
+ 
+    private ScheduleEvent event = new DefaultScheduleEvent();
+ 
+    @PostConstruct
+    public void init() {
+        eventModel = new DefaultScheduleModel();            
+    }
+     
+    
+     
+    public Date getInitialDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.FEBRUARY, calendar.get(Calendar.DATE), 0, 0, 0);
+         
+        return calendar.getTime();
+    }
+     
+    public ScheduleModel getEventModel() {
+        return eventModel;
+    }
+     
+ 
+    public ScheduleEvent getEvent() {
+        return event;
+    }
+ 
+    public void setEvent(ScheduleEvent event) {
+        this.event = event;
+    }
+     
+    public void addEvent(ActionEvent actionEvent) {
+        if(event.getId() == null)
+            eventModel.addEvent(event);
+        else
+            eventModel.updateEvent(event);
+         
+        event = new DefaultScheduleEvent();
+        System.out.println(event.getStartDate()+" - "+ event.getEndDate());
+    }
+     
+    public void onEventSelect(SelectEvent selectEvent) {
+        event = (ScheduleEvent) selectEvent.getObject();
+    }
+     
+    public void onDateSelect(SelectEvent selectEvent) {
+        event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
+        System.out.println(event.getStartDate()+" - "+ event.getEndDate());
+        System.out.println((Date) selectEvent.getObject()+" - "+ (Date) selectEvent.getObject());
+        
+    }
+     
+    public void onEventMove(ScheduleEntryMoveEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+         
+        addMessage(message);
+    }
+     
+    public void onEventResize(ScheduleEntryResizeEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+         
+        addMessage(message);
+    }
+     
+    private void addMessage(FacesMessage message) {
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
